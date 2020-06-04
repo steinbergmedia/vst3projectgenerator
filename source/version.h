ca9214540775467d48b0b767b721ca99c6eab2db
scheffle <scheffle@users.noreply.github.com>
2020-06-04 12:11:59 +0200

    Change Version number

--- source/version.h
+++ source/version.h
@@ -2,11 +2,11 @@
 
 #include "version_buildnumber.h"
 
-#define MAJOR_VERSION_STR "0"
-#define MAJOR_VERSION_INT 0
+#define MAJOR_VERSION_STR "2020"
+#define MAJOR_VERSION_INT 2020
 
-#define SUB_VERSION_STR "1"
-#define SUB_VERSION_INT 1
+#define SUB_VERSION_STR "6"
+#define SUB_VERSION_INT 6
 
 #define RELEASE_NUMBER_STR "0"
 #define RELEASE_NUMBER_INT 0
